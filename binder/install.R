84f85205e7cedd70a442846a4f8175b551710d63
wicky-dot <62839062+wicky-dot@users.noreply.github.com>
2020-09-01 23:48:11 +0800

    Update install.R

--- binder/install.R
+++ binder/install.R
@@ -5,9 +5,12 @@ install.packages("ggplot2") # for plotting
 install.packages(c("rmarkdown", "caTools", "bitops")) # for knitting
 
 ### install bioconductor packages
-# install.packages("BiocManager")
-# BiocManager::install("package")
+ install.packages("BiocManager")
+ BiocManager::install("package")
 
 ### install GitHub packages (tag = commit, branch or release tag)
-# install.packages("devtools")
-# devtools::install_github("user/repo", ref = "tag")
+ install.packages("devtools")
+ devtools::install_github("user/repo", ref = "tag")
+
+### install excel
+install.packages("readxl")
